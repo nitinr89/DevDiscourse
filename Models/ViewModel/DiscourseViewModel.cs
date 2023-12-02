1a06c45349352bc7942af5e33edca629e6d82876
Uttam Kumar <uttamk@visionri.com>
2023-12-02 17:02:23 +0530

    News, Bolgs, BreakingNews, LiveDiscource, Interview

--- Models/ViewModel/DiscourseViewModel.cs
+++ Models/ViewModel/DiscourseViewModel.cs
@@ -26,7 +26,8 @@ namespace Devdiscourse.Models.ViewModel
         }
         private string RemoveAccent(string text)
         {
-            byte[] bytes = System.Text.Encoding.GetEncoding("Cyrillic").GetBytes(text);
+            //byte[] bytes = System.Text.Encoding.GetEncoding("Cyrillic").GetBytes(text);
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(text);
             return System.Text.Encoding.ASCII.GetString(bytes);
         }
     }

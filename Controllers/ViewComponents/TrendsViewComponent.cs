9902727c70876a15251632dbd071024fa4335c5d
Danish Jamal <djamal@visionri.com>
2024-01-06 09:56:45 +0530

    Changed anchortags on view component UI

--- Controllers/ViewComponents/TrendsViewComponent.cs
+++ Controllers/ViewComponents/TrendsViewComponent.cs
@@ -73,23 +73,26 @@ namespace DevDiscourse.Controllers.ViewComponents
                 }
                 else
                 {
-                    //var resultList = _db.DevNews.Where(a => a.AdminCheck == true && a.Sector != "14" && a.CreatedOn < todayDate && a.CreatedOn > weekend && a.Id != id && a.Region.Contains(reg) && a.IsSponsored == false).OrderByDescending(a => a.ViewCount)
-                    //    .Select(a => new LatestNewsView
-                    //    {
-                    //        Id = a.Id,
-                    //        Title = a.Title,
-                    //        CreatedOn = a.ModifiedOn,
-                    //        ImageUrl = a.ImageUrl,
-                    //        Sector = a.Sector,
-                    //        Country = a.Country,
-                    //        NewId = a.NewsId,
-                    //        Type = a.Type,
-                    //        SubType = a.SubType,
-                    //        Label = a.NewsLabels
-                    //    }).Take(4)
-                    //    //.AsNoTracking()
-                    //    .ToList();
-                    //return View(resultList);
+                    var resultList = _db.DevNews.Where(a => a.AdminCheck == true && a.Sector != "14" && a.CreatedOn < todayDate && a.CreatedOn > weekend &&
+                    a.Id != id 
+                    //&& a.Region.Contains(reg)
+                    && a.IsSponsored == false).OrderByDescending(a => a.ViewCount)
+                        .Select(a => new LatestNewsView
+                        {
+                            Id = a.Id,
+                            Title = a.Title,
+                            CreatedOn = a.ModifiedOn,
+                            ImageUrl = a.ImageUrl,
+                            Sector = a.Sector,
+                            Country = a.Country,
+                            NewId = a.NewsId,
+                            Type = a.Type,
+                            SubType = a.SubType,
+                            Label = a.NewsLabels
+                        }).Take(4)
+                        //.AsNoTracking()
+                        .ToList();
+                    return View(resultList);
 
                     //var infocus = (from dn in _db.DevNews
                     //               select new LatestNewsView
@@ -109,35 +112,35 @@ namespace DevDiscourse.Controllers.ViewComponents
                     //      .ToList();
                     //return View(infocus.GroupBy(s => s.Title).Select(a => a.FirstOrDefault()).OrderByDescending(o => o.Ranking).Take(6).ToList());
 
-                    var resultList = _db.DevNews
-    .Where(dn => dn.AdminCheck == true &&
-              dn.CreatedOn < todayDate && dn.CreatedOn > weekend && dn.CreatedOn < todayDate && dn.Id != id &&
-              dn.Sector == "14")
-    .OrderByDescending(dn => dn.CreatedOn)
-    .Take(65)
-    .Select(dn => new LatestNewsView
-    {
-        Id = dn.Id,
-        NewId = dn.NewsId,
-        Title = dn.Title,
-        ImageUrl = dn.ImageUrl,
-        CreatedOn = dn.ModifiedOn,
-        Type = dn.Type,
-        SubType = dn.SubType,
-        Country = dn.Country,
-        Label = dn.NewsLabels,
-        Ranking = 0
-    })
-    .ToList();
+                    //                var resultList = _db.DevNews
+                    //.Where(dn => dn.AdminCheck == true &&
+                    //          dn.CreatedOn < todayDate && dn.CreatedOn > weekend && dn.CreatedOn < todayDate && dn.Id != id &&
+                    //          dn.Sector == "14")
+                    //.OrderByDescending(dn => dn.CreatedOn)
+                    //.Take(65)
+                    //.Select(dn => new LatestNewsView
+                    //{
+                    //    Id = dn.Id,
+                    //    NewId = dn.NewsId,
+                    //    Title = dn.Title,
+                    //    ImageUrl = dn.ImageUrl,
+                    //    CreatedOn = dn.ModifiedOn,
+                    //    Type = dn.Type,
+                    //    SubType = dn.SubType,
+                    //    Country = dn.Country,
+                    //    Label = dn.NewsLabels,
+                    //    Ranking = 0
+                    //})
+                    //.ToList();
 
-                    var groupedResult = resultList
-                        .GroupBy(s => s.Title)
-                        .Select(group => group.OrderByDescending(a => a.Ranking).FirstOrDefault())
-                        .OrderByDescending(o => o.Ranking)
-                        .Take(5)
-                        .ToList();
+                    //                var groupedResult = resultList
+                    //                    .GroupBy(s => s.Title)
+                    //                    .Select(group => group.OrderByDescending(a => a.Ranking).FirstOrDefault())
+                    //                    .OrderByDescending(o => o.Ranking)
+                    //                    .Take(5)
+                    //                    .ToList();
 
-                    return View(groupedResult);
+                    //                return View(groupedResult);
                 }
             }catch(Exception ex)
             {

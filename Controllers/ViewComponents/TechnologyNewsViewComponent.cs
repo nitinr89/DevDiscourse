3807f229a526ff2222e190b24721385652d33f26
Danish Jamal <djamal@visionri.com>
2024-01-24 11:31:41 +0530

    reverted query on homepage

--- Controllers/ViewComponents/TechnologyNewsViewComponent.cs
+++ Controllers/ViewComponents/TechnologyNewsViewComponent.cs
@@ -20,24 +20,24 @@ namespace Devdiscourse.Controllers.ViewComponents
             try
             {
                 ViewBag.Sector = "6";
-                DateTime twoMonth = DateTime.UtcNow.AddDays(-120);
+                DateTime twoMonth = DateTime.UtcNow.AddDays(-2);
                 //not working
 
-                //var result = _db.RegionNewsRankings.Where(a => a.DevNews.CreatedOn > twoMonth && a.DevNews.AdminCheck == true && a.DevNews.Sector == "6" && a.Region.Title == reg && a.DevNews.IsSponsored == false).OrderByDescending(a => a.DevNews.CreatedOn).Select(a => new NewsViewModel
-                //{
-                //    Title = a.DevNews.Title,
-                //    NewsId = a.DevNews.NewsId,
-                //    ImageUrl = a.DevNews.ImageUrl,
-                //    Subtitle = a.DevNews.SubTitle,
-                //    Country = a.DevNews.Country,
-                //    CreatedOn = a.DevNews.ModifiedOn,
-                //    Sector = a.DevNews.Type,
-                //    SubType = a.DevNews.SubType,
-                //    Label = a.DevNews.NewsLabels,
-                //    Ranking = a.Ranking
-                //}).AsNoTracking().Take(30).ToList();
+                var result = _db.RegionNewsRankings.Where(a => a.DevNews.CreatedOn > twoMonth && a.DevNews.AdminCheck == true && a.DevNews.Sector == "6" && a.Region.Title == reg && a.DevNews.IsSponsored == false).OrderByDescending(a => a.DevNews.CreatedOn).Select(a => new NewsViewModel
+                {
+                    Title = a.DevNews.Title,
+                    NewsId = a.DevNews.NewsId,
+                    ImageUrl = a.DevNews.ImageUrl,
+                    Subtitle = a.DevNews.SubTitle,
+                    Country = a.DevNews.Country,
+                    CreatedOn = a.DevNews.ModifiedOn,
+                    Sector = a.DevNews.Type,
+                    SubType = a.DevNews.SubType,
+                    Label = a.DevNews.NewsLabels,
+                    Ranking = a.Ranking
+                }).AsNoTracking().Take(30).ToList();
 
-                //return View( result.GroupBy(s => s.Title).Select(a => a.FirstOrDefault()).OrderByDescending(a => a.Ranking).Take(6));
+                return View(result.GroupBy(s => s.Title).Select(a => a.FirstOrDefault()).OrderByDescending(a => a.Ranking).Take(6));
 
                 //working but not optimised
 
@@ -60,36 +60,37 @@ namespace Devdiscourse.Controllers.ViewComponents
                 //return View(infocus.GroupBy(s => s.Title).Select(a => a.FirstOrDefault()).OrderByDescending(o => o.Ranking).Take(6).ToList());
 
                 //working 
-                var resultList = _db.DevNews
-     .Where(dn => dn.AdminCheck == true &&
-                  dn.CreatedOn > twoMonth &&
-                  dn.Sector == "6")
-     .OrderByDescending(dn => dn.CreatedOn)
-     .Take(65)
-     .Select(dn => new NewsViewModel
-     {
-         NewsId = dn.NewsId,
-         Title = dn.Title,
-         ImageUrl = dn.ImageUrl,
-         CreatedOn = dn.ModifiedOn,
-         Subtitle = dn.SubTitle,
-         SubType = dn.SubType,
-         Country = dn.Country,
-         Sector = dn.Sector,
-         Label = dn.NewsLabels,
-         Ranking = 0
-     })
-     .ToList();
+                //           var resultList = _db.DevNews
+                //.Where(dn => dn.AdminCheck == true &&
+                //             dn.CreatedOn > twoMonth &&
+                //             dn.Sector == "6")
+                //.OrderByDescending(dn => dn.CreatedOn)
+                //.Take(65)
+                //.Select(dn => new NewsViewModel
+                //{
+                //    NewsId = dn.NewsId,
+                //    Title = dn.Title,
+                //    ImageUrl = dn.ImageUrl,
+                //    CreatedOn = dn.ModifiedOn,
+                //    Subtitle = dn.SubTitle,
+                //    SubType = dn.SubType,
+                //    Country = dn.Country,
+                //    Sector = dn.Sector,
+                //    Label = dn.NewsLabels,
+                //    Ranking = 0
+                //})
+                //.ToList();
 
-                var groupedResult = resultList
-                    .GroupBy(s => s.Title)
-                    .Select(group => group.OrderByDescending(a => a.Ranking).FirstOrDefault())
-                    .OrderByDescending(o => o.Ranking)
-                    .Take(30)
-                    .ToList();
+                //           var groupedResult = resultList
+                //               .GroupBy(s => s.Title)
+                //               .Select(group => group.OrderByDescending(a => a.Ranking).FirstOrDefault())
+                //               .OrderByDescending(o => o.Ranking)
+                //               .Take(30)
+                //               .ToList();
 
-                return View(groupedResult);
-            }catch(Exception ex)
+                //           return View(groupedResult);
+            }
+            catch(Exception ex)
             {
                 return Content("Error: " + ex.Message);
             }

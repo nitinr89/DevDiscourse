6e2b7b96dd00f2a32a5c641c16a9b8cf43622743
Danish Jamal <djamal@visionri.com>
2024-02-21 14:55:19 +0530

    code refactor part two

--- Controllers/AgencyWireController.cs
+++ Controllers/AgencyWireController.cs
@@ -98,95 +98,63 @@ namespace Devdiscourse.Controllers
             ViewBag.type = type;
             return View();
         }
-        //public PartialViewResult GetNewsAnalysisItems(string region, string type, int? page)
-        //{
-        //    int pageSize = 25;
-        //    int pageNumber = (page ?? 1);
-        //    ViewBag.page = pageNumber;
-        //    DateTime oneMonth = pageNumber == 1 ? DateTime.Today.AddDays(-2) : DateTime.Today.AddDays(-15);
-        //    if (type == "Other" || region == "Global Edition")
-        //    {
-        //        //var search =  _db.RegionNewsRankings.Where(a=> a.DevNews.AdminCheck == true && a.DevNews.CreatedOn> oneMonth).Select(a=> new SearchView
-        //        //{
-        //        //    Id = a.DevNews.Id,
-        //        //    NewsId = a.DevNews.NewsId,
-        //        //    Title = a.DevNews.Title,
-        //        //    ImageUrl = a.DevNews.ImageUrl,
-        //        //    Country = a.DevNews.Country,
-        //        //    CreatedOn = a.DevNews.ModifiedOn,
-        //        //    Region = a.DevNews.Region,
-        //        //    Sector = a.DevNews.Sector,
-        //        //    IsGlobal = a.DevNews.IsGlobal,
-        //        //    IsVideo = a.DevNews.IsVideo,
-        //        //    IsSponsored = a.DevNews.IsSponsored,
-        //        //    EditorPick = a.DevNews.EditorPick,
-        //        //    Tags = a.DevNews.Tags,
-        //        //    Type = a.DevNews.Type,
-        //        //    SubType = a.DevNews.SubType,
-        //        //    Category = a.DevNews.Category,
-        //        //    Label = a.DevNews.NewsLabels,
-        //        //    Ranking =  a.Ranking
-        //        //}).GroupBy(a=>a.Title).Select(s=>s.FirstOrDefault()).AsNoTracking().OrderByDescending(o => o.CreatedOn).ThenByDescending(s => s.Ranking).ToPagedList(pageNumber, pageSize);
-        //        var search = (from a in _db.DevNews
-        //                      //where a.AdminCheck && a.CreatedOn > oneMonth
-        //                      select new NewsAnalysisViewModel
-        //                      {
-        //                          NewsId = a.NewsId,
-        //                          Title = a.Title,
-        //                          ImageUrl = a.ImageUrl,
-        //                          Country = a.Country,
-        //                          CreatedOn = a.ModifiedOn,
-        //                          Type = a.Type,
-        //                          SubType = a.SubType,
-        //                          Label = a.NewsLabels
-        //                      })
-        //                      //.OrderByDescending(o => o.CreatedOn).AsNoTracking()
-        //                      .ToPagedList(pageNumber, pageSize);
-        //        return PartialView("_getNewsAnalysisItems", search);
-        //    }
-        //    else
-        //    {
-        //        //var search = (from a in _db.DevNews
-        //        //              where a.AdminCheck == true && a.CreatedOn > oneMonth && a.Region.Contains(region)
-        //        //               select new SearchView
-        //        //              {
-        //        //                  Id = a.Id,
-        //        //                  NewsId = a.NewsId,
-        //        //                  Title = a.Title,
-        //        //                  ImageUrl = a.ImageUrl,
-        //        //                  Country = a.Country,
-        //        //                  CreatedOn = a.ModifiedOn,
-        //        //                  Region = a.Region,
-        //        //                  Sector = a.Sector,
-        //        //                  IsGlobal = a.IsGlobal,
-        //        //                  IsVideo = a.IsVideo,
-        //        //                  IsSponsored = a.IsSponsored,
-        //        //                  EditorPick = a.EditorPick,
-        //        //                  Tags = a.Tags,
-        //        //                  Type = a.Type,
-        //        //                  SubType = a.SubType,
-        //        //                  Category = a.Category,
-        //        //                  Label = a.NewsLabels
-        //        //              }).OrderByDescending(o => o.CreatedOn).AsNoTracking().ToPagedList(pageNumber, pageSize);
-        //        NewsAnalysisvar search = _db.RegionNewsRankings
-        //            //.Where(a => a.DevNews.AdminCheck == true && a.Region.Title == region && a.DevNews.CreatedOn > oneMonth)
-        //            .Select(a => new NewsAnalysisViewModel
-        //        {
-        //            NewsId = a.DevNews.NewsId,
-        //            Title = a.DevNews.Title,
-        //            ImageUrl = a.DevNews.ImageUrl,
-        //            Country = a.DevNews.Country,
-        //            CreatedOn = a.DevNews.ModifiedOn,
-        //            Type = a.DevNews.Type,
-        //            SubType = a.DevNews.SubType,
-        //            Label = a.DevNews.NewsLabels,
-        //            Ranking = a.Ranking
-        //        })
-        //            //.AsNoTracking().OrderByDescending(o => o.CreatedOn)
-        //            .ToPagedList(pageNumber, pageSize);
-        //        return PartialView("_getNewsAnalysisItems"/*, search.OrderByDescending(o => o.CreatedOn.Date).ThenByDescending(s => s.Ranking).AsEnumerable()*/);
-        //    }
-        //}
+        public PartialViewResult GetNewsAnalysisItems(string region, string type, int? page)
+        {
+            int pageSize = 25;
+            int pageNumber = (page ?? 1);
+            ViewBag.page = pageNumber;
+            DateTime oneMonth = pageNumber == 1 ? DateTime.Today.AddDays(-2) : DateTime.Today.AddDays(-15);
+            if (type == "Other" || region == "Global Edition")
+            {
+
+                var search = (from a in _db.DevNews
+                              where a.AdminCheck && a.CreatedOn > oneMonth
+                              select new NewsAnalysisViewModel
+                              {
+                                  NewsId = a.NewsId,
+                                  Title = a.Title,
+                                  ImageUrl = a.ImageUrl,
+                                  Country = a.Country,
+                                  CreatedOn = a.ModifiedOn,
+                                  Type = a.Type,
+                                  SubType = a.SubType,
+                                  Label = a.NewsLabels
+                              })
+                              .OrderByDescending(o => o.CreatedOn).AsNoTracking()
+                              .ToPagedList(pageNumber, pageSize);
+                return PartialView("_getNewsAnalysisItems", search);
+            }
+            else
+            {
+                var reg = (from c in _db.Countries
+                           join r in _db.Regions on c.RegionId equals r.Id
+                           where c.Title == region
+                           select new
+                           {
+                               r.Title
+                           }).FirstOrDefault();
+                string regionTitle = "Global Edition";
+                var userRegion = reg != null && reg.Title != null ? regionTitle = reg.Title : regionTitle = region;
+
+                var search = _db.RegionNewsRankings
+                    .Where(a => a.DevNews.AdminCheck == true && a.Region.Title == userRegion && a.DevNews.CreatedOn > oneMonth)
+                    .Select(a => new NewsAnalysisViewModel
+                    {
+                        NewsId = a.DevNews.NewsId,
+                        Title = a.DevNews.Title,
+                        ImageUrl = a.DevNews.ImageUrl,
+                        Country = a.DevNews.Country,
+                        CreatedOn = a.DevNews.ModifiedOn,
+                        Type = a.DevNews.Type,
+                        SubType = a.DevNews.SubType,
+                        Label = a.DevNews.NewsLabels,
+                        Ranking = a.Ranking
+                    })
+                    .AsNoTracking().OrderByDescending(o => o.CreatedOn)
+                    .ToPagedList(pageNumber, pageSize);
+                return PartialView("_getNewsAnalysisItems", search.OrderByDescending(o => o.CreatedOn.Date).ThenByDescending(s => s.Ranking).AsEnumerable());
+            }
+        }
 
         public PartialViewResult GetNews(int skip = 0, int take = 0)
         {
--- Controllers/ViewComponents/InfocusViewComponent.cs
+++ Controllers/ViewComponents/InfocusViewComponent.cs
@@ -18,51 +18,29 @@ namespace DevDiscourse.Controllers.ViewComponents
 
             try
             {
-
-
                 var lastThreeHour = DateTime.UtcNow.AddDays(-12);
-                //var infocus = _db.RegionNewsRankings
-                //  .Where(a => a.DevNews.AdminCheck == true
-                //              && a.Region.Title == reg
-                //              && a.DevNews.CreatedOn > lastThreeHour
-                //              && !a.DevNews.Title.Contains("News Summary")
-                //              && !a.DevNews.Title.Contains("Highlights")
-                //              && a.DevNews.NewsLabels != "Newsalert"
-                //              && !new[] { "14", "18", "19", "9" }.Contains(a.DevNews.Sector))
-                //  .Select(s => new LatestNewsView
-                //  {
-                //      Id = dn.Id,
-                //      NewId = dn.NewsId,
-                //      Title = dn.Title,
-                //      ImageUrl = dn.ImageUrl,
-                //      CreatedOn = dn.ModifiedOn,
-                //      Type = dn.Type,
-                //      SubType = dn.SubType,
-                //      Country = dn.Country,
-                //      Label = dn.NewsLabels,
-                //      Ranking = s.Ranking
-                //  }).OrderByDescending(a => a.CreatedOn)
-                //      .Take(65)
-                //      .ToList();
-
-
-                var infocus = (from dn in _db.DevNews
-                               where dn.AdminCheck == true && dn.CreatedOn > lastThreeHour && dn.NewsLabels != "Newsalert"
+                var infocus = (from a in _db.RegionNewsRankings
+                               where a.DevNews.AdminCheck == true
+                                  && a.Region.Title == reg
+                                  && a.DevNews.CreatedOn > lastThreeHour
+                                  && !a.DevNews.Title.Contains("News Summary")
+                                  && !a.DevNews.Title.Contains("Highlights")
+                                  && a.DevNews.NewsLabels != "Newsalert"
+                                  && !new[] { "14", "18", "19", "9" }.Contains(a.DevNews.Sector)
+                               orderby a.DevNews.ModifiedOn descending
                                select new LatestNewsView
                                {
-                                   Id = dn.Id,
-                                   NewId = dn.NewsId,
-                                   Title = dn.Title,
-                                   ImageUrl = dn.ImageUrl,
-                                   CreatedOn = dn.ModifiedOn,
-                                   Type = dn.Type,
-                                   SubType = dn.SubType,
-                                   Country = dn.Country,
-                                   Label = dn.NewsLabels,
-                                   Ranking = 0
-                               }).OrderByDescending(a => a.CreatedOn)
-                      .Take(65)
-                      .ToList();
+                                   Id = a.DevNews.Id,
+                                   NewId = a.DevNews.NewsId,
+                                   Title = a.DevNews.Title,
+                                   ImageUrl = a.DevNews.ImageUrl,
+                                   CreatedOn = a.DevNews.ModifiedOn,
+                                   Type = a.DevNews.Type,
+                                   SubType = a.DevNews.SubType,
+                                   Country = a.DevNews.Country,
+                                   Label = a.DevNews.NewsLabels,
+                                   Ranking = a.Ranking
+                               }).Take(65).ToList();
                 return View(infocus.GroupBy(s => s.Title).Select(a => a.FirstOrDefault()).OrderByDescending(o => o.Ranking).Take(6).ToList());
             }
             catch (Exception ex)
@@ -70,37 +48,7 @@ namespace DevDiscourse.Controllers.ViewComponents
                 return Content("Error: " + ex.Message);
 
             }
-            //            var resultList = _db.DevNews
-            //.Where(dn => dn.AdminCheck == true &&
-            //         dn.CreatedOn > lastThreeHour && !dn.Title.Contains("News Summary")
-            //                          && !dn.Title.Contains("Highlights")
-            //                          && dn.NewsLabels != "Newsalert"
-            //                          && !new[] { "14", "18", "19", "9" }.Contains(dn.Sector) )
-            //.OrderByDescending(dn => dn.CreatedOn)
-            //.Take(65)
-            //.Select(dn => new NewsViewModel
-            //{
-            //    NewsId = dn.NewsId,
-            //    Title = dn.Title,
-            //    ImageUrl = dn.ImageUrl,
-            //    CreatedOn = dn.ModifiedOn,
-            //    Subtitle = dn.SubTitle,
-            //    SubType = dn.SubType,
-            //    Country = dn.Country,
-            //    Sector = dn.Sector,
-            //    Label = dn.NewsLabels,
-            //    Ranking = 0
-            //})
-            //.ToList();
-
-            //            var groupedResult = resultList
-            //                .GroupBy(s => s.Title)
-            //                .Select(group => group.OrderByDescending(a => a.Ranking).FirstOrDefault())
-            //                .OrderByDescending(o => o.Ranking)
-            //                .Take(30)
-            //                .ToList();
-
-            //            return View(groupedResult);
+            
         }
     }
 }

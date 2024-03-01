85ba209ece01ba99d92750f11dcf227d53f34a49
Danish Jamal <djamal@visionri.com>
2024-03-01 16:23:37 +0530

    tag news query updated

--- Controllers/ViewComponents/NewsItemsViewComponent.cs
+++ Controllers/ViewComponents/NewsItemsViewComponent.cs
@@ -23,24 +23,42 @@ namespace Devdiscourse.Controllers.ViewComponents
                 cat = cat ?? "";
                 int pageSize = 20;
                 int pageNumber = (page ?? 1);
-                var resultList = (from dn in _db.DevNews.AsNoTracking()
-                                  where dn.AdminCheck == true && dn.NewsLabels == label && dn.IsSponsored == false
-                                  select new NewsViewModel
-                                  {
-                                      NewsId = dn.NewsId,
-                                      Title = dn.Title,
-                                      ImageUrl = dn.ImageUrl,
-                                      CreatedOn = dn.ModifiedOn,
-                                      Subtitle = dn.SubTitle,
-                                      Sector = dn.Sector,
-                                      SubType = dn.SubType,
-                                      Country = dn.Country,
-                                      Label = dn.NewsLabels,
-                                      Ranking = 0
-                                  }).OrderByDescending(a => a.CreatedOn)
-               .Take(65)
-               .ToList();
-                return View(resultList.GroupBy(s => s.Title).Select(a => a.FirstOrDefault()).OrderByDescending(o => o.Ranking).Take(6).ToList());
+                // var resultList = (from dn in _db.DevNews.AsNoTracking()
+                //                   where dn.AdminCheck == true && dn.NewsLabels == label && dn.IsSponsored == false
+                //                   select new NewsViewModel
+                //                   {
+                //                       NewsId = dn.NewsId,
+                //                       Title = dn.Title,
+                //                       ImageUrl = dn.ImageUrl,
+                //                       CreatedOn = dn.ModifiedOn,
+                //                       Subtitle = dn.SubTitle,
+                //                       Sector = dn.Sector,
+                //                       SubType = dn.SubType,
+                //                       Country = dn.Country,
+                //                       Label = dn.NewsLabels,
+                //                       Ranking = 0
+                //                   }).OrderByDescending(a => a.CreatedOn)
+                //.Take(65)
+                //.ToList();
+                // return View(resultList.GroupBy(s => s.Title).Select(a => a.FirstOrDefault()).OrderByDescending(o => o.Ranking).ToList());
+                var resultList = _db.RegionNewsRankings.AsNoTracking().
+                    Where(a => a.DevNews.AdminCheck == true && a.DevNews.NewsLabels == label
+                    && a.Region.Title == region &&
+                    a.DevNews.IsSponsored == false).OrderByDescending(a => a.DevNews.CreatedOn).Select(a => new NewsViewModel
+                    {
+                        Title = a.DevNews.Title,
+                        NewsId = a.DevNews.NewsId,
+                        ImageUrl = a.DevNews.ImageUrl,
+                        Subtitle = a.DevNews.SubTitle,
+                        Country = a.DevNews.Country,
+                        CreatedOn = a.DevNews.ModifiedOn,
+                        Sector = a.DevNews.Type,
+                        SubType = a.DevNews.SubType,
+                        Label = a.DevNews.NewsLabels,
+                        Ranking = a.Ranking
+                    }).ToPagedList(pageNumber, pageSize);
+
+                return View(resultList.OrderByDescending(s => s.CreatedOn.Date).ThenByDescending(o => o.Ranking));
             }
             catch (Exception ex)
             {

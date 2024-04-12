3f760d09c45f02fcbb65e1500acc7383c3cffa1d
Uttam Kumar <uttamk@visionri.com>
2024-04-12 17:10:18 +0530

    unused open ai deleted

--- Models/OpenAI.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Devdiscourse.Models
-{
-    public class OpenAIModel
-    {
-        public required string Question { get; set; }
-        public string Answer { get; set; } = string.Empty;
-    }
-}
